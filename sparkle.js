00fa74601f143b532d4498b1ca948013f3943e81
Kate Wellington <sharedferret@gmail.com>
2012-01-18 12:29:35 -0500

    Added unique song count and number of DJs to stats command

--- sparkle.js
+++ sparkle.js
@@ -686,11 +686,18 @@ bot.on('speak', function (data) {
 		//in the room
 		case 'stats':
 			if (config.useDatabase) {
-				client.query('SELECT count(*) as total, sum(up) as up, avg(up) as avgup, '
+				client.query('SELECT @uniquesongs := count(*) FROM (select * from '
+					+ config.SONG_TABLE + ' group by concat(song, \' by \', artist)) as songtbl');
+				client.query('SELECT @numdjs := count(*) FROM (select * from '
+					+ config.SONG_TABLE + ' group by djid) as djtable');
+				client.query('SELECT @uniquesongs as uniquesongs, @numdjs as numdjs, '
+					+ 'count(*) as total, sum(up) as up, avg(up) as avgup, '
 					+ 'sum(down) as down, avg(down) as avgdown FROM ' + config.SONG_TABLE,
 					function select(error, results, fields) {
 						bot.speak('In this room, '
-							+ results[0]['total'] + ' songs have been played with a total of '
+							+ results[0]['total'] + ' songs ('
+							+ results[0]['uniquesongs'] + ' unique) have been played by '
+							+ results[0]['numdjs'] + ' DJs with a total of '
 							+ results[0]['up'] + ' awesomes and ' + results[0]['down']
 							+ ' lames (avg +' + new Number(results[0]['avgup']).toFixed(1) 
 							+ '/-' + new Number(results[0]['avgdown']).toFixed(1)
