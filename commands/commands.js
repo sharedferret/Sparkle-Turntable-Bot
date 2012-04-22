b364db7499893a69213e9eb39223d7424c5cadca
Vineet Kumar <vineet@doorstop.net>
2012-04-21 20:12:25 -0400

    Simplify commands listing with filter and join instead of a loop.

--- commands/commands.js
+++ commands/commands.js
@@ -5,11 +5,8 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
-    var response = 'Commands: ';
-    for (i in commands) {
-        if (commands[i].enabled && !(commands[i].hidden)) {
-            response += commands[i].name + ', ';
-        }
-    }
+    var response = 'Commands: ' + commands.filter(function(command) {
+        return command.enabled && !command.hidden;
+    }).join(', ');
     output({text: response.substring(0, response.length - 2), destination: data.source, userid: data.userid});
 }
