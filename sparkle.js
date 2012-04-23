8d72cd439559e932952cc5f2fb5ff495b19327c8
Kate Wellington <sharedferret@gmail.com>
2012-04-23 19:59:50 -0400

    Bump version

--- sparkle.js
+++ sparkle.js
@@ -14,7 +14,7 @@
  *
 */
 var args = process.argv;
-global.version = '[Sparkle] Version 1.0b2';
+global.version = '[Sparkle] Version 1.0b3';
 
 global.fs = require('fs');
 global.url = require('url'); 
