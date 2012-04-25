da47a0c873044c686fe326438e180d2596686522
yayramen <daltongore@gmail.com>
2012-04-24 23:09:06 -0700

    updated earlier method

--- commands/commands.js
+++ commands/commands.js
@@ -7,6 +7,6 @@ exports.matchStart = false;
 exports.handler = function(data) {
     var response = 'Commands: ' + commands.filter(function(command) {
         return command.enabled && !command.hidden;
-    }).map(function(command){return command.name;}).join(', ');
-    output({text: response.substring(0, response.length - 2), destination: data.source, userid: data.userid});
+    }).map(function(command){return command.name;}).sort().join(', ');
+    output({text: response, destination: data.source, userid: data.userid});
 }
