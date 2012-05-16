49aedc773ab042ab51f2640c67d964e6928976ea
Kate Wellington <sharedferret@gmail.com>
2012-05-16 10:19:49 -0400

    Bot will only fan if a user fans them first
    This command requires ttapi v1.4.1 or higher.

--- commands/fanme.js
+++ commands/fanme.js
@@ -3,7 +3,21 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
-    bot.becomeFan(data.userid);
-    var response = ('Fanned you, ' + data.name + '!');
-    output({text: response, destination: data.source, userid: data.userid});
+    bot.getFans(function (fandata) {
+        var response = '';
+        if (fandata.fans.filter(function(fan) { return fan == data.userid; }).length == 0) {
+            response += 'Fan me first, ' + data.name + '!';
+            output({text: response, destination: data.source, userid: data.userid});
+        } else {
+            bot.becomeFan(data.userid, function(becomefan) {
+                if (becomefan.success) {
+                    response = 'Fanned you, ' + data.name + '!';
+                } else {
+                    response = 'I\'ve already fanned you, ' + data.name + '!';
+                }
+                output({text: response, destination: data.source, userid: data.userid});
+            });
+
+        }
+    });
 }
