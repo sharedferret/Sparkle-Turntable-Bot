4532797fb6c4a329228d9caa496ed4ea42c2afc6
Kate Wellington <sharedferret@gmail.com>
2012-05-23 14:35:06 -0400

    Made .weather 20% cooler

--- commands/forecast.js
+++ commands/forecast.js
@@ -1,9 +1,9 @@
-exports.name = '.weather';
+exports.name = '.forecast';
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data) {
-    var userlocation = data.text.substring(9);
+    var userlocation = data.text.substring(10);
     if (userlocation == '') {
         userlocation = 20151;
     }
@@ -11,15 +11,17 @@ exports.handler = function(data) {
     request('http://www.google.com/ig/api?weather=' + encodeURIComponent(userlocation),
         function cb(error, response, body) {
         parser.parseString(body, function(err, result) {
-            if (result.weather.forecast_information != null) {
+            if (result.weather.forecast_conditions != null) {
                 try {
-                    var rp = 'The weather in ' 
-                        + result.weather.forecast_information.city['@'].data
-                        + ' is ' + result.weather.current_conditions.temp_f["@"].data
-                        + '°F and ' + result.weather.current_conditions.condition["@"].data
-                        + ' (' + result.weather.current_conditions.wind_condition["@"].data
-                        + ', ' + result.weather.current_conditions.humidity["@"].data
-                        + ').';
+                    var rp = 'Forecast for ' + result.weather.forecast_information.city['@'].data + ': ';
+                    for (i in result.weather.forecast_conditions) {
+                        rp += result.weather.forecast_conditions[i].day_of_week["@"].data
+                            + ': '
+                            + result.weather.forecast_conditions[i].condition["@"].data
+                            + ' (' + result.weather.forecast_conditions[i].high["@"].data
+                            + '°/' + result.weather.forecast_conditions[i].low["@"].data
+                            + '°). ';
+                    }
                     output({text: rp, destination: data.source, userid: data.userid});
                 } catch (e) {
                     output({text: 'An error occurred.', destination: data.source, userid: data.userid});
