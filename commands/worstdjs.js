92bada83759c2f18248d33a730d2fee0c06574ce
Kate Wellington <sharedferret@gmail.com>
2013-03-17 22:35:58 -0700

    Fix worstdjs

--- commands/worstdjs.js
+++ commands/worstdjs.js
@@ -4,14 +4,14 @@ exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
     if (config.database.usedb) {
-        db.all('select username, upvotes from (select djid, sum(down) as upvotes from '
+        db.all('select username, downvotes from (select djid, sum(down) as downvotes from '
 			+ config.database.tablenames.song + ' group by djid order by sum(down) desc '
 			+ 'limit 3) a inner join (select * from ' + config.database.tablenames.user
-			+ ') b on a.djid = b.userid order by upvotes desc limit 3',
+			+ ') b on a.djid = b.userid order by downvotes desc limit 3',
             function select(error, rows) {
                 var response = 'The DJs with the most lames accrued in this room: ';
                 for (i in rows) {
-                    response += results[i]['username'] + ': '
+                    response += rows[i]['username'] + ': '
                         + rows[i]['downvotes'] + ' lames.  ';
                 }
                 output({text: response, destination: data.source, userid: data.userid});
