09bb0f71cd94cbcc6e34bc43f6d55a995408dff5
Kate Wellington <sharedferret@gmail.com>
2013-03-24 17:08:19 -0700

    Implement mystats

--- commands/mystats.js
+++ commands/mystats.js
@@ -4,27 +4,41 @@ exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
     if (config.database.usedb) {
-        //These two statements gets the user's rank (by awesomes) and sets it to @rank
-        db.run('SET @rownum := 0');
-        db.run('SELECT @rank := rank FROM (SELECT @rownum := @rownum + 1 AS '
-            + 'rank, djid, POINTS FROM (SELECT djid, sum(up) as POINTS from '
-            + config.database.dbname + '.' + config.database.tablenames.song
-            + ' group by djid order by sum(up) desc) as test) as rank where '
-            + 'djid like \'' + data.userid + '\'');
-        //This statement grabs the rank from the previous query, and gets the total songs
-        //played, total awesomes, lames, and averages
-        db.all('SELECT @rank as rank, count(*) as total, sum(up) as up, avg(up) as avgup, '
-            + 'sum(down) as down, avg(down) as avgdown '
-            + 'FROM '+ config.database.dbname + '.' + config.database.tablenames.song + ' WHERE `djid` LIKE \''
-            + data.userid + '\'',
-            function select(error, results, fields) {
-                var response = (data.name + ', you have played ' + results[0]['total'] 
-                    + ' songs in this room with a total of '
-                    + results[0]['up'] + ' awesomes and ' + results[0]['down']
-                    + ' lames (avg +' + new Number(results[0]['avgup']).toFixed(1) 
-                    + '/-' + new Number(results[0]['avgdown']).toFixed(1)
-                    + ') (Rank: ' + results[0]['rank'] + ')');
-                output({text: response, destination: data.source, userid: data.userid});
-        });
-    }
+		async.waterfall([
+			function(callback) {
+				db.all('select count(*) as total, sum(up) as sumup, sum(down) as sumdown, ' 
+					+ 'avg(up) as avgup, avg(down) as avgdown from ' 
+					+ config.database.tablenames.song
+					+ ' where djid = \'' + data.userid + '\' group by djid',
+					function(error, results) {
+						if (results.length == 0) {
+							error = 'No results found!';
+						}
+						callback(error, results[0]);
+					});
+			},
+			function(response, callback) {
+				db.all('select count(*) as rank from (select sum(up) as sumup from '
+					+ config.database.tablenames.song + ' group by djid order by sumup '
+					+ 'desc) as a where sumup >= ' + response['sumup'],
+				function(error, results) {
+						response['rank'] = results[0]['rank'];
+						callback(error, response);
+					});
+			}
+		], function(err, result) {
+			var response = '';
+			if (err) {
+				response = data.name + ', you haven\'t played any songs in here yet!';
+			} else {
+				response = (data.name + ', you have played ' + result['total'] 
+                    + ' songs in this room with a total of ' + result['sumup'] 
+					+ ' awesomes and ' + result['sumdown'] + ' lames (avg +' 
+					+ new Number(result['avgup']).toFixed(1) + '/-' 
+					+ new Number(result['avgdown']).toFixed(1)
+                    + ') (Rank: ' + result['rank'] + ')');
+			}
+            output({text: response, destination: data.source, userid: data.userid});
+		});
+	}
 }
